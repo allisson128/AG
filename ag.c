de34b1a591ebae7b7f889b47ace376077295d361
Allisson A. A. Garcia <allisson128@gmail.com>
2015-09-10 11:49:51 -0300

    fim do basico

--- ag.c
+++ ag.c
@@ -8,10 +8,11 @@
 #include <string.h>
 
 #define POPULATION_SIZE 50
+#define EVAL_REFERENCE  100000
 
 int    randsingular (int *vet, int size, int edge);   /* gera individuo aleatorio, sem repeticao de inteiros */
 int    contain (int* vet, int element, int size);     /* verifica se ha o elemeento no vetor */
-void   copy (int* vet1, int* vet2, int size);
+void   copy (int* destino, int* origem, int size);
 char** initEvalAuxVector (char* A, char* B, char* C); /* Esse eh aquele vetor de letras nao repetidas */
 int    evalAplusBequalC (int *vet);                   /* func. de aval. A + B = C */
 int    roulette(int** population, int populationsize, int col, int value);
@@ -27,15 +28,16 @@ int main(int argc, char** argv) {
   // CONSTANTES
   int    newpopulationsize, populationsize = 50;
   float  crossoverrate = .2, mutationrate = .02;
-  int    numero_geracoes = 1;
+  int    numero_geracoes = 100;
 
   // Tentar fazer generico
   int    domain_size = 10;
   int    i, j, acc,it;
+  int    optimal = -1;
   /* char** map; */
 
   newpopulationsize = populationsize * crossoverrate;
-  printf("\nnewPopSize = %d\n", newpopulationsize);
+  /* printf("\nnewPopSize = %d\n", newpopulationsize); */
   srand(time(NULL));
 
   /* if (argc != 4) { */
@@ -55,43 +57,55 @@ int main(int argc, char** argv) {
   acc = 0;
   for (i = 0; i < populationsize; ++i) {
     acc += originalpopulation[i][10] = evalAplusBequalC (originalpopulation[i]);
+
+    if (originalpopulation[i][10] == EVAL_REFERENCE) {
+      optimal = i;
+    }
+
     originalpopulation[i][11] = acc;
     //imprimeVet(originalpopulation[i], 12);
   }
 
-  for (j = 0; j < numero_geracoes; ++j) {
+  for (j = 0; (j < numero_geracoes) && (optimal < 0); ++j) {
 
-    /* CROSSOVER */
-    /* Roleta */
-
+    
     for (it = 0; it < newpopulationsize; it++) {
-      /* SELECAO */
+
+      /*************************** SELECAO ******************************/
+      /* Roleta */
       int index_pai1 = roulette(originalpopulation, populationsize, 11, acc);
       int index_pai2 = roulette(originalpopulation, populationsize, 11, acc);
+      /******************************************************************/
 
+      /*************************** CROSSOVER ******************************/
       crossover_simple(newpopulation, it, originalpopulation, index_pai1, index_pai2);
-      // imprimeVet(originalpopulation[index_pai1], 10);
-      // imprimeVet(originalpopulation[index_pai2], 10);
-      // imprimeVet(newpopulation[2*it], 10);
-      // imprimeVet(newpopulation[2*it+1], 10);
-      // putchar('\n');
+      /********************************************************************/
+
+      /* imprimeVet(originalpopulation[index_pai1], 10); */
+      /* imprimeVet(originalpopulation[index_pai2], 10); */
+      /* imprimeVet(newpopulation[2*it], 10); */
+      /* imprimeVet(newpopulation[2*it+1], 10); */
+      /* putchar('\n'); */
     }
 		
-    /* MUTACAO */
+    /*************************** MUTACAO ******************************/
     permutation(newpopulation, newpopulationsize, mutationrate);
+    /******************************************************************/
 
-  	/* ATRIBUI NOTAS - Avalia populacao de filhos */
-  	acc = 0;
-  	for (i = 0; i < newpopulationsize; ++i) {
-    	acc += newpopulation[i][10] = evalAplusBequalC (newpopulation[i]);
-    	newpopulation[i][11] = acc;
-    	imprimeVet(newpopulation[i], 12);
-  	}
-  	
+    /***** AVALIA os novos indiv. apos Crossover e Mutacao ************/
+    acc = 0;
+    for (i = 0; i < newpopulationsize; ++i) {
+      acc += newpopulation[i][10] = evalAplusBequalC (newpopulation[i]);
+      newpopulation[i][11] = acc;
+      /* imprimeVet(newpopulation[i], 12); */
+    }
+    /******************************************************************/
+
+    /****** SELECIONA os melhores indiv. dentre os pais e filhos ******/
     int** temp = init_population(10);
     for(i = 0; i < 50; i++) {
 
-      int valor_maior = -1, maior = 0,x,y, matrix = 0;
+      int x, y, valor_maior = -1, maior = 0, matrix = 0;
 
       for(x = 0; x < populationsize; ++x) {
         if(originalpopulation[x][10] > valor_maior) {
@@ -109,13 +123,43 @@ int main(int argc, char** argv) {
         }
       }
 
-      if(matrix)
+      if (matrix) {
         copy(temp[i], newpopulation[maior], 12);
-      else
-        copy(temp[i], originalpopulation[maior], 12);
-    }
+	newpopulation[maior][10] = 0;
 
+	if (temp[i][10] == EVAL_REFERENCE) {
+	  optimal = i;
+	}
+
+      }
+      else {
+        copy(temp[i], originalpopulation[maior], 12);
+	originalpopulation[maior][10] = 0;
+
+	if (temp[i][10] == EVAL_REFERENCE) {
+	  optimal = i;
+	}
+
+      }
+    }
+    /****************************************************************/
+    for (i = 0; i < domain_size; ++i) {
+      free(originalpopulation[i]);
+    }
+    free(originalpopulation);
+    originalpopulation = temp;
   }
+
+  for (i = 0; i < populationsize; ++i) {
+    imprimeVet(originalpopulation[i], 12);
+  }
+
+
+  if (optimal >= 0) {
+    printf("\nSucesso - Optimal = %d\n", optimal);
+  }
+
+
   return 0;
 }
 
@@ -180,8 +224,10 @@ int evalAplusBequalC (int *vet) {
   if (A + B == C) {
     printf("\n\n!!! ENCONTROU !!!\nSend + More = Money\n\n");
   }
-
-  return abs(C - A + B);
+  /* printf("\nc = %d, a = %d, b = %d", C, A, B); */
+  /* printf("\nc - a + b = %d", abs(C - (A + B))); */
+  /* printf("\n100000 - abs(c - a + b) = %d\n", EVAL_REFERENCE - abs(C - (A + B))); */
+  return EVAL_REFERENCE - abs(C - (A + B));
 }
 
 int roulette(int** population, int populationsize, int col, int value) {
@@ -223,31 +269,31 @@ int** init_population(int individual_size) {
 }
 
 void crossover_simple(int** newpopulation, int it, int** originalpopulation, int index_pai1, int index_pai2) {
-	int i, corte = rand() % 10;
-	it *= 2;
-	for (i = 0; i < corte; ++i) {
-		newpopulation[it][i] = originalpopulation[index_pai1][i];
-		newpopulation[it + 1][i] = originalpopulation[index_pai2][i];
-	}
-	for (i = corte; i < 10; ++i) {
-		newpopulation[it][i] = originalpopulation[index_pai2][i];
-		newpopulation[it + 1][i] = originalpopulation[index_pai1][i];
-	}
+  int i, corte = rand() % 10;
+  it *= 2;
+  for (i = 0; i < corte; ++i) {
+    newpopulation[it][i] = originalpopulation[index_pai1][i];
+    newpopulation[it + 1][i] = originalpopulation[index_pai2][i];
+  }
+  for (i = corte; i < 10; ++i) {
+    newpopulation[it][i] = originalpopulation[index_pai2][i];
+    newpopulation[it + 1][i] = originalpopulation[index_pai1][i];
+  }
 }
 
 void permutation(int** newpopulation, int newpopulationsize, float mutationrate) {
-	int i, aux, qtd = (int) newpopulationsize * mutationrate;
-	int rnd, rnd1, rnd2;
+  int i, aux, qtd = (int) newpopulationsize * mutationrate;
+  int rnd, rnd1, rnd2;
 
-	for (i = 0; i < qtd; ++i) {
-		rnd  = rand() % newpopulationsize;
-		rnd1 = rand() % 10;
-		rnd2 = rand() % 10;
+  for (i = 0; i < qtd; ++i) {
+    rnd  = rand() % newpopulationsize;
+    rnd1 = rand() % 10;
+    rnd2 = rand() % 10;
 
-		aux = newpopulation[rnd][rnd1];
-		newpopulation[rnd][rnd1] = newpopulation[rnd][rnd2];
-		newpopulation[rnd][rnd2] = aux;
-	}
+    aux = newpopulation[rnd][rnd1];
+    newpopulation[rnd][rnd1] = newpopulation[rnd][rnd2];
+    newpopulation[rnd][rnd2] = aux;
+  }
 
 }
 /* RASCUNHO:
